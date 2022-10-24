ef926f899d66cb64439cdb85c43d276b58ee2045
unknown <mdtanvir6070@gmail.com>
2022-10-24 21:27:15 +0600

    dashboard, banner, user data modify done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 var cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(express.json());
 app.use(cors());
@@ -15,18 +15,113 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
 // ? Connection setup
 
 async function run() {
   try {
     await client.connect();
-    const collection = client.db("test").collection("devices");
+    const userCollection = client.db("FeriBD").collection("users");
+    const bannerCollection = client.db("FeriBD").collection("banners");
+
+    //   ? User data
+    app.get("/getAllUsers", async (req, res) => {
+      const result = await (await userCollection.find().toArray()).reverse();
+      res.send(result);
+    });
+
+    app.post("/addUser", async (req, res) => {
+      const data = req.body;
+      const email = data.email;
+      const find = { email: email };
+      const filter = await userCollection.findOne(find);
+      if (filter === null) {
+        const result = await userCollection.insertOne(data);
+        res.send(result);
+      }
+    });
+
+    // app.put("/updateUser", async (req, res) => {
+    //   const email = req.body.email;
+    //   const data = req.body;
+    //   const find = { email: email };
+    //   const filter = await userCollection.findOne(find);
+    //   if (filter) {
+    //     res.status(403).send("Email already exist");
+    //   }
+    //   if (!filter) {
+    //     const option = { upsert: true };
+    //     const updateDoc = {
+    //       $set: data,
+    //     };
+    //     const result = await userCollection.updateOne(
+    //       filter,
+    //       option,
+    //       updateDoc
+    //     );
+    //     res.send(result);
+    //   }
+    // });
+
+    app.put("/changeRole/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const data = req.body;
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: data,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, option);
+      res.send(result);
+    });
+
+    app.delete("/deleteUser/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    //   ? Banner data
+
+    app.get("/getAllBanners", async (req, res) => {
+      const result = await (await bannerCollection.find().toArray()).reverse();
+      res.send(result);
+    });
+
+    app.get("/getCheckedBanners", async (req, res) => {
+      const find = await bannerCollection.find().toArray();
+      const result = find.filter((c) => c.checked === true);
+      res.send(result);
+    });
+
+    app.post("/addBanner", async (req, res) => {
+      const data = req.body;
+      const result = await bannerCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.put("/updateBanner/:id", async (req, res) => {
+      const data = req.body;
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: data,
+      };
+      const result = await bannerCollection.updateOne(
+        filter,
+        updateDoc,
+        option
+      );
+      res.send(result);
+    });
+
+    app.delete("/deleteBanner/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await bannerCollection.deleteOne(filter);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
